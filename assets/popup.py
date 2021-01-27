2cd0a4265dd32d07fd6c008861e9f35b0ce792db
Michael Ndon <manj19.mjn@gmail.com>
2021-01-27 18:13:40 +0100

    updated git ignore; added assets folder for file explorer

---
+++ assets/popup.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import simpledialog
+
+def popup(head, prompt):        
+    ROOT = tk.Tk()
+    ROOT.withdraw()
+    USER_INP = simpledialog.askstring(title=head, prompt=prompt)
+    return USER_INP
+
+def passpopup(head, prompt):        
+    ROOT = tk.Tk()
+    ROOT.withdraw()
+    USER_INP = simpledialog.askstring(title=head, prompt=prompt, show='*')
+    return USER_INP
+
+
